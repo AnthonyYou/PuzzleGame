aa63ba2c7c77a169da5f81082ae854f2b654936e
Roberto Capelo <rfcapelo@gmail.com>
2013-03-12 18:25:50 -0500

    many changes

---
+++ PuzzleGame/EndGame.h
@@ -0,0 +1,13 @@
+//
+//  EndGame.h
+//  PuzzleGame
+//
+//  Created by Roberto Capelo on 07/02/13.
+//  Copyright (c) 2013 Voice Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EndGame : AFHTTPClient
+
+@end
---
+++ PuzzleGame/EndGame.m
@@ -0,0 +1,13 @@
+//
+//  EndGame.m
+//  PuzzleGame
+//
+//  Created by Roberto Capelo on 07/02/13.
+//  Copyright (c) 2013 Voice Technology. All rights reserved.
+//
+
+#import "EndGame.h"
+
+@implementation EndGame
+
+@end
